774467a2d52a57f609f28ba8b172a2eff6572f0e
Sarah Schmoller <sarah.schmoller@gmail.com>
2023-12-18 17:43:26 -0600

    Updated mobile handling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -43,7 +43,7 @@ function HomePage() {
     let contactPage = document.getElementById('contactPage');
     let lastScrollY = window.scrollY;
 
-    isMobile = false;
+    isMobile = isMobile();
     if (isMobile) {
 
       handleMobileMenu()
